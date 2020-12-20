1e909b80500101270605f1e2429d2d40397ea3d2
baraa <natourb@post.bgu.ac.il>
2020-12-20 20:25:39 +0200

    prepare server api  for first iteration in home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -10,6 +10,8 @@ export class HomePageComponent implements OnInit {
 
   constructor(private accounts: AccountsService) { }
   userFirstName = '';
+  front_to_server = JSON.stringify({"username":"[the string the user inserted]","password":"[sha256 hash for the string inserted by user ]"})
+  login_server_to_front = JSON.stringify({"name":"[user Full Name]", "token":"[string from server per session]", "username":"[user username]"})
   ngOnInit(): void {
     this.userFirstName = this.accounts.get_user_firs_name()
     // this.userFirstName = JSON.parse(localStorage.getItem('user')).firstName;
